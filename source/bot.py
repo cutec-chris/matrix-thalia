17308d49b997f1275304d3cd12ca988a0bbffb5d
Christian U <christian@ullihome.de>
2022-04-21 06:32:37 +0200

    updated basic stuff

--- source/bot.py
+++ source/bot.py
@@ -1,10 +1,11 @@
+#https://github.com/DS4A-team34/ds4a_application/blob/736c69e002cf4a46f83cbd8c522ee6b0029f0793/common6.py
 from init import *
-import rcon,a2s
-from types import MappingProxyType
-servers = []
-loop = None
+import spacy
 nlp = spacy.load("de_core_news_sm")
 @bot.listener.on_message_event
+class Client(Config):
+    def __init__(self, room, **kwargs) -> None:
+        super().__init__(room, **kwargs) 
 async def tell(room, message):
     global servers,lastsend
     match = botlib.MessageMatch(room, message, bot, prefix)
@@ -26,16 +27,19 @@ async def check_server(server):
             else:
                 await bot.api.send_text_message(server['room'],str(e))
         await asyncio.sleep(1)
+try:
+    with open('data.json', 'r') as f:
+        nservers = json.load(f)
+        for server in nservers:
+            servers.append(Server(server))
+except BaseException as e: 
+    logging.error('Failed to read config.yml:'+str(e))
+    exit(1)
 @bot.listener.on_startup
 async def startup(room):
     global loop,servers
     loop = asyncio.get_running_loop()
-    try:
-        with open('data.json', 'r') as f:
-            servers = json.load(f)
-    except: pass
     for server in servers:
-        server = MappingProxyType(server)
         if server['room'] == room:
             loop.create_task(check_server(server))
 @bot.listener.on_message_event
