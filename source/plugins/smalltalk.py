f49cc727f69012d6438231e5824016f1d603ddd2
Christian U <christian@ullihome.de>
2022-04-23 13:15:34 +0200

    actual changes

--- source/plugins/smalltalk.py
+++ source/plugins/smalltalk.py
@@ -1,11 +1,11 @@
 import plugin
 async def CheckSentence(s,User,ForceAnswer=False):
     if s[:3]=='en ':
-        words = plugin.analyse_sentence(s[3:])
+        words = plugin.analyse_sentence(s[3:],'en')
     else:
         words = plugin.analyse_sentence(s)
     if words:
-        res = ''
+        res = 'text vector pos tag lemma tag label\n'
         for token in words:
-            res += token.text+' '+str(token.vector_norm)+' '+token.pos_+' '+token.tag_+'\n'
+            res += token.text+' '+str(token.vector_norm)+' '+token.pos_+' '+token.tag_+' '+token.lemma_+' '+token.tag_+' '+token.ent_type_+'\n'
         return {'text':res}
